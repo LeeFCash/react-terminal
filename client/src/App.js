85c51a0a58bd551f5a257e4330edbc8fbf8fc48f
Lee Cash <118697195+LeeFCash@users.noreply.github.com>
2023-02-27 21:50:25 +0000

    start

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+/*function terminal() {
+  return (
+    <div className="terminal">
+        <p>
+          what up
+        </p>
+    </div>
+  );
+}
+*/
+
+function App() {
+  return (
+    <div className="App">
+        <p>t</p>
+    </div>
+  );
+}
+
+export default App;
